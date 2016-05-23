f12bfab86f9b1bbd248a70c8eacbb13f3646ed58
Paulo Ricardo <rochapaulo@MacBook-Pro-de-Paulo.local>
2016-05-23 00:15:26 -0300

    reorganizing

---
+++ bin/bot.js
@@ -0,0 +1,17 @@
+
+'use strict';
+
+var MastermindBot = require('../lib/mastermind.js');
+
+var token = process.env.BOT_API_KEY;
+var name  = process.env.BOT_NAME;
+var game_api_uri = process.env.GAME_API_URI;
+
+var mastermindbot = new MastermindBot({
+    token: token, 
+    name: name,
+    game_api_uri: game_api_uri
+});
+
+console.log('starting mastermind-bot')
+mastermindbot.run();
